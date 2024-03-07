a677d252a8aeafb2d95da417ea7c990f971481e1
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-03-07 19:49:00 +0000

    calculate volume of cone program

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // input
+  const radius = parseInt(document.getElementById("radius-of-cone").value)
+  const height = parseInt(document.getElementById("height-of-cone").value)
+
+  // process
+  const volume = 1/3 * (Math.PI) * (radius * radius) * height
+  // const volume = radius + height // not proper forumla, using to see if process works
+
+  // output
+  document.getElementById("volume-of-cone").innerHTML = volume.toFixed() + " cm²"
+}
